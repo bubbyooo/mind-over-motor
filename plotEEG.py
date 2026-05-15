cd5af0a59948b2501ae58270ee9dab57f2cc357c
fionastern28 <fionastern28@gmail.com>
2026-05-14 21:17:08 -0400

    add function to plot EEG channels:

---
+++ plotEEG.py
@@ -0,0 +1,20 @@
+import torch
+import matplotlib.pyplot as plt 
+
+# full definition from claude
+def plot(data):
+    scale = torch.quantile(data.abs(), 0.95).item() * 2
+    offsets = torch.arange(data.shape[0] - 1, -1, -1) * scale
+    fig, ax = plt.subplots(figsize=(15, 8))
+    for i in range(data.shape[0]):
+        ax.plot((data[i] + offsets[i]).numpy(), linewidth=1)
+
+    ax.set_yticks(offsets.numpy())
+    ax.set_yticklabels(['Cz', 'C3', 'C4'])
+    ax.set_xticks(ax.get_xticks())
+    ax.set_xticklabels([int(x * 2) for x in ax.get_xticks()])
+    ax.set_xlabel("Time (ms)")
+    ax.set_ylabel("EEG channels")
+    ax.set_xlim(0, data.shape[1])
+    plt.tight_layout()
+    plt.show()
