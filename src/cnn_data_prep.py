f326e0105f29c628fe7810dccdae596bef9fd516
fionastern28 <fionastern28@gmail.com>
2026-05-18 21:10:32 -0400

    remove print statements and add split fraction parameter

--- src/cnn_data_prep.py
+++ src/cnn_data_prep.py
@@ -5,13 +5,13 @@ sys.path.append(str(Path(__file__).parent.parent / 'src'))
 import torch
 from dataset import random_split
 
-def data_prep(dataset):
+def data_prep(dataset, fraction = 0.8):
     """
     Prepares train/test tensors from the dataset.
     Adds a combin
     Splits data, clamps outliers, and normalizes using training set statistics.
     """
-    train, test = random_split(dataset)
+    train, test = random_split(dataset, frac = fraction)
 
     # Stack list of samples into tensors
     X_train = torch.stack([item["x"] for item in train]).float()
@@ -39,6 +39,5 @@ def add_polynomial_feat(X):
     # adds a feature channel multiplying channel c3 by channel c4
     c3_c4 = X[:, 1, :] * X[:, 2, :]
     c3_c4 = torch.unsqueeze(c3_c4, dim = 1)
-    print("XSHAPE: ", X.shape, " COMBO SHAPE: ", c3_c4.shape)
     X = torch.cat((X, c3_c4), dim = 1)
     return X
